63948c829fcee42b675b268e3dbcee17164694b3
Quop Murga <laurentemurga@gmail.com>
2021-12-16 05:38:47 +0000

    Finish github library

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -41,7 +41,8 @@ class Dev(Configuration):
       'django.contrib.sessions',
       'django.contrib.messages',
       'django.contrib.staticfiles',
-      'movies'
+      'movies',
+      'gh'
   ]
 
   MIDDLEWARE = [
@@ -59,7 +60,7 @@ class Dev(Configuration):
   TEMPLATES = [
       {
           'BACKEND': 'django.template.backends.django.DjangoTemplates',
-          'DIRS': [],
+          'DIRS': ['templates'],
           'APP_DIRS': True,
           'OPTIONS': {
               'context_processors': [
